bb1ad99456669751dcc8f35bc6f41dc072ac4afd
Toby-van-Berkum <s1131370@student.hsleiden.nl>
2024-06-04 16:48:20 +0200

    Added Migrations

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,23 +7,24 @@
     <title>Document</title>
 </head>
 <body class="bg-slate-200">
-    <header class="bg-sky-500 py-4 container mx-auto flex justify-between items-center">
-            <a href="#" class="text-white text-xl font-bold flex items-center">
-                <figure>
-                    <img src="{{ asset('SadBread.png') }}" alt="Time2Share Logo" class="h-10 mr-2 px-2">
-                </figure>
-                Time2Share
-            </a>
-            <nav class="flex items-center">
-            <ul class="flex space-x-4">
-                <li><a href="#" class="text-white hover:text-gray-200">Home</a></li>
-                <li><a href="#" class="text-white hover:text-gray-200">Browse</a></li>
-                <li><a href="#" class="text-white hover:text-gray-200">About</a></li>
-                <li><a href="#" class="text-white hover:text-gray-200">Contact</a></li>
-            </ul>
-            <a href="#" class="text-white hover:text-gray-200 ml-4">Login</a>
-            <a href="#" class="bg-white text-blue-500 px-4 py-2 rounded-full hover:bg-gray-200 transition duration-300 ml-4">Sign Up</a>
-            </nav>
+    <header class="bg-sky-500 py-4 container flex justify-between items-center">
+        <a href="#" class="text-white text-xl font-bold flex items-center">
+            <figure>
+                <img src="{{ asset('SadBread.png') }}" alt="Time2Share Logo" class="h-10 mr-2 px-2">
+            </figure>
+            Time2Share
+        </a>
+        <nav class="flex items-center mx-4">
+        <ul class="flex space-x-4">
+            <li><a href="#" class="text-white hover:text-gray-200">Home</a></li>
+            <li><a href="#" class="text-white hover:text-gray-200">Browse</a></li>
+            <li><a href="#" class="text-white hover:text-gray-200">About</a></li>
+            <li><a href="#" class="text-white hover:text-gray-200">Contact</a></li>
+            <li><a href="#" class="text-white hover:text-gray-200">Login</a></li>
+            <li><a href="#" class="bg-white text-blue-500 px-4 py-2 rounded-full hover:bg-gray-200 transition duration-300">Sign Up</a></li>
+        </ul>
+        </nav>
+        
     </header>
 
     <main>
